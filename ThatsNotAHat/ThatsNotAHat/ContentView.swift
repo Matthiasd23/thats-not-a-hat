60348f018a87a032ec2125baf633b165c1abd2d0
anjescc <annija434@gmail.com>
2024-02-21 11:28:13 +0100

    setup

---
+++ ThatsNotAHat/ThatsNotAHat/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ThatsNotAHat
+//
+//  Created by UsabilityLab on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThatsNotAHat/ThatsNotAHat/ThatsNotAHatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThatsNotAHatApp.swift
+//  ThatsNotAHat
+//
+//  Created by UsabilityLab on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ThatsNotAHatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
